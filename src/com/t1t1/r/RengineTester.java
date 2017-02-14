653a4abab8956d4a461d28f4269baaa378b529e6
t1t1 <cha@hanmail2.net>
2017-02-14 13:31:53 +0900

    Add R Code parser

--- src/com/t1t1/r/RengineTester.java
+++ src/com/t1t1/r/RengineTester.java
@@ -1,63 +1,85 @@
 package com.t1t1.r;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.rosuda.JRI.Rengine;
 
 public class RengineTester {
 
 	public static void main(String[] args) {
-		
-//		rJava 설치
-//
-//		install.packages("rJava")
-//		library(rJava)
-//
-//		#환경변수 설정
-//		#바탕화면-컴퓨터-마오-속성-고급시스템설정
-//		#-환경변수-시스템변수-새로만들기
-//
-//		#1)
-//		#R_HOME 변수 등록 (R-tool위치)
-//		#(예) C:\Program Files\R\R-3.3.2
-//
-//		#2)
-//		#시스템변수 Path 경로 지정
-//		#(rJava 패키지를 설치한 프로그램의 위치)
-//		#64bit : %R_HOME%\bin\x64; %R_HOME%\library\rJava\jri\x64;
-//		#C:\Program Files\R\R-3.3.2\bin\x64;C:\Program Files\R\R-3.3.2\library\rJava\jri\x64;
-//
-//		#rJava
-//		#JRI는 Java 애플리케이션에서 R을 실행할 수 있는
-//		#Java-R 인터페이스
-//		#Java에 R 동적 라이브러리를 로드하고
-//		#R에 Java API를 제공
-//
-//		#Eclipse 설정
-//		#1.자바 프로젝트 하나 생성-Rjava1
-//		#2.생성된 프로젝트를 navigator 보기로 한후 다음,
-//		#프로젝트 아래 lib폴더 하나 만들고
-//		#C:\Program Files\R\R-3.3.2\library\rJava\jri 경로에 있는 3개 JAR 파일
-//		#(JRI.jar, JRIEngine.jar, REngine.jar)을 모두 복사한다.
-//		#3. 프로젝트-마오-property- Java Build Path-Library 탭에서
-//		#Add JARs 하여 프로젝트 lib 방에 있는 jar를 모두 선택한다
-//
-//		#rJava 프로그램 작성하기		
-//		
-//		by KMS
 
-		Rengine re1 = new Rengine(null, false, null);
-		if (!re1.waitForR()) {
-			System.out.println("에러났다는~~~");
+		Rengine rengine = getRengine();
+
+		checkRengine(rengine);
+
+		executeRCode(rengine, "static/RCode.txt", "C:/R");
+
+	}
+
+	private static Rengine getRengine() {
+		Rengine rengine = new Rengine(null, false, null);
+		return rengine;
+	}
+
+	private static void checkRengine(Rengine rengine) {
+		if (!rengine.waitForR()) {
+			System.out.println("ERROR: rengine.waitForR()");
 			System.exit(1);
 		}
+	}
 
-		System.out.println("start");
-		re1.eval("x<-c(1:10)");
-		re1.eval("jpeg(filename=\"C:/R/rJava2.jpg\")");
-		re1.eval("pie(x)");
-		re1.eval("dev.off()");
-		re1.end();
-		System.out.println("end");
+	private static void executeRCode(Rengine rengine, String inputPath, String outputPath) {
+        
+		BufferedReader br = null;
+		FileReader fr = null;
 		
+		try {
+			File data = new File(inputPath);
+			fr = new FileReader(data);
+			br = new BufferedReader(fr);
+			String line;
+			while ( (line = br.readLine()) != null) {
+				
+				// 코드 치환
+				if (line.indexOf("{{outputPath}}") > -1) {
+					line = line.replace("{{outputPath}}", outputPath);
+				}
+				if (line.indexOf("{{now}}") > -1) {
+					line = line.replace("{{now}}", getNow());
+				}
+				
+				// execute
+				rengine.eval(line);
+				System.out.println(line);
+				
+				if (line.isEmpty()) {
+					rengine.end();
+					break;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null) br.close();
+				if (fr != null) fr.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		
+	}
+
+	private static String getNow() {
+		Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_hhmmss");
+        String now = sdf.format(date);
+		return now;
 	}
 	
 }
